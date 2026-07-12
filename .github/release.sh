3b77f6cb98a7461f1c2df7c95886fe42d8b5fe28
Kwisma <77215384+Kwisma@users.noreply.github.com>
2026-07-12 18:01:00 +0800

    fix: 修复 release 版本读取

--- .github/release.sh
+++ .github/release.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+set -e
 
 current_tag=$(git describe --tags --abbrev=0)
-previous_tag=$(git describe --tags --abbrev=0 "$current_tag^")
-version_range="$previous_tag...$current_tag"
+
+previous_tag=$(git describe --tags --abbrev=0 "$current_tag^" 2>/dev/null || echo "")
+
+if [ -n "$previous_tag" ]; then
+	version_range="$previous_tag...$current_tag"
+else
+	version_range="$current_tag"
+fi
 
 if [ -n "$GITHUB_REPOSITORY" ]; then
 	repo=$GITHUB_REPOSITORY
@@ -14,7 +21,13 @@ fi
 
 {
 	echo "## What's Changed"
-	git log "$version_range" --pretty=format:"* %h %s"
+
+	if [ -n "$previous_tag" ]; then
+		git log "$version_range" --pretty=format:"* %h %s"
+	else
+		git log "$current_tag" --pretty=format:"* %h %s"
+	fi
+
 	echo
 	echo
 	echo "**Full Changelog**: https://github.com/$repo/compare/$version_range"
